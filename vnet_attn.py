c7b306d7d5bcdd02322930d0480343c74a7bf1e5
Hsu <hsuxu820@gmail.com>
2018-10-11 20:25:24 +0800

    Update vnet_attn.py

--- vnet_attn.py
+++ vnet_attn.py
@@ -5,27 +5,6 @@ import numpy as np
 
 from blocks import *
 
-class OutputBlock(nn.Module):
-    """ output block of v-net
-
-        The output is a list of foreground-background probability vectors.
-        The length of the list equals to the number of voxels in the volume
-    """
-
-    def __init__(self, in_channels, out_channels):
-        super(OutputBlock, self).__init__()
-        self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm3d(out_channels)
-        self.act1 = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size=1)
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, input):
-        out = self.act1(self.bn1(self.conv1(input)))
-        out = self.conv2(out)
-        out = self.softmax(out)
-
-
 def vnet_kaiming_init(net):
     net.apply(kaiming_weight_init)
 

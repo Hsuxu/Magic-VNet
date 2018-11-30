b6579ec6b759c5346eb8b4c9f89c429db2046b9c
Hsu <hsuxu820@gmail.com>
2018-11-30 16:21:38 +0800

    add SENet in VNet

--- blocks.py
+++ blocks.py
@@ -4,8 +4,12 @@ import torch.nn.functional as F
 
 
 class ConvBnRelu3(nn.Module):
-    def __init__(self, in_channels, out_channels, ksize, padding, do_act=True):
+    """ classic combination: conv + batch normalization [+ relu] """
+
+    def __init__(self, in_channels, out_channels, ksize=3, padding=1, do_act=True):
         super(ConvBnRelu3, self).__init__()
+        if isinstance(ksize, tuple) and padding is None:
+            padding = tuple(item // 2 for item in ksize)
         self.conv = nn.Conv3d(in_channels, out_channels, kernel_size=ksize, padding=padding, groups=1)
         self.bn = nn.BatchNorm3d(out_channels)
         self.do_act = do_act
@@ -20,6 +24,8 @@ class ConvBnRelu3(nn.Module):
 
 
 class BottConvBnRelu3(nn.Module):
+    """Bottle neck structure"""
+
     def __init__(self, channels, ratio, do_act=True):
         super(BottConvBnRelu3, self).__init__()
         self.conv1 = ConvBnRelu3(channels, channels // ratio, ksize=1, padding=0, do_act=True)
@@ -31,12 +37,39 @@ class BottConvBnRelu3(nn.Module):
         return out
 
 
+class SeBlock(nn.Module):
+    """
+    reference:
+    Squeeze-and-Excitation Networks
+    Dual Attention Network for Scene Segmentation
+    channel wise attention still
+    """
+
+    def __init__(self, channels, ratio=8):
+        super(SeBlock, self).__init__()
+        # if not channels//ratio >0:
+        #     raise ValueError
+        self.avg_pool = nn.AdaptiveAvgPool3d(1)
+        self.fc = nn.Sequential(nn.Linear(channels, channels // ratio),
+                                nn.LeakyReLU(),
+                                nn.Linear(channels // ratio, channels),
+                                nn.Sigmoid())
+
+    def forward(self, input):
+        bs, chn, dim, hei, wid = input.size()
+        scale = self.avg_pool(input)
+        scale = scale.view(bs, -1)
+        scale = self.fc(scale)
+        scale = scale.view(bs, chn, 1, 1, 1)
+        return scale * input
+
+
 class ResidualBlock3(nn.Module):
     """ residual block with variable number of convolutions """
 
-    def __init__(self, channels, ksize, padding, num_convs):
+    def __init__(self, channels, ksize, padding, num_convs, use_se=False):
         super(ResidualBlock3, self).__init__()
-
+        self.use_se = use_se
         layers = []
         for i in range(num_convs):
             if i != num_convs - 1:
@@ -46,18 +79,24 @@ class ResidualBlock3(nn.Module):
 
         self.ops = nn.Sequential(*layers)
         self.act = nn.ReLU(inplace=True)
+        if self.use_se:
+            self.se_block = SeBlock(channels, ratio=8)
+            self.gamma = nn.Parameter(torch.zeros(1))
 
     def forward(self, input):
-
         output = self.ops(input)
+        if self.use_se:
+            output = self.se_block(output)
+            output = self.gamma * output
         return self.act(input + output)
 
 
 class BottResidualBlock3(nn.Module):
     """ block with bottle neck conv"""
 
-    def __init__(self, channels, ratio, num_convs):
+    def __init__(self, channels, ratio, num_convs, use_se=False):
         super(BottResidualBlock3, self).__init__()
+        self.use_se = use_se
         layers = []
         for i in range(num_convs):
             if i != num_convs - 1:
@@ -67,9 +106,15 @@ class BottResidualBlock3(nn.Module):
 
         self.ops = nn.Sequential(*layers)
         self.act = nn.ReLU(inplace=True)
+        if self.use_se:
+            self.se_block = SeBlock(channels, ratio=8)
+            self.gamma = nn.Parameter(torch.zeros(1))
 
     def forward(self, input):
         output = self.ops(input)
+        if self.use_se:
+            output = self.se_block(output)
+            output = self.gamma * output
         return self.act(input + output)
 
 
@@ -90,16 +135,16 @@ class InputBlock(nn.Module):
 class DownBlock(nn.Module):
     """ downsample block of v-net """
 
-    def __init__(self, in_channels, num_convs, use_bottle_neck=False):
+    def __init__(self, in_channels, num_convs, use_bottle_neck=False, use_se=False):
         super(DownBlock, self).__init__()
         out_channels = in_channels * 2
         self.down_conv = nn.Conv3d(in_channels, out_channels, kernel_size=2, stride=2, groups=1)
         self.down_bn = nn.BatchNorm3d(out_channels)
         self.down_act = nn.ReLU(inplace=True)
         if use_bottle_neck:
-            self.rblock = BottResidualBlock3(out_channels, 4, num_convs)
+            self.rblock = BottResidualBlock3(out_channels, 4, num_convs, use_se=use_se)
         else:
-            self.rblock = ResidualBlock3(out_channels, 3, 1, num_convs)
+            self.rblock = ResidualBlock3(out_channels, 3, 1, num_convs, use_se=use_se)
 
     def forward(self, input):
         out = self.down_act(self.down_bn(self.down_conv(input)))
@@ -110,15 +155,15 @@ class DownBlock(nn.Module):
 class UpBlock(nn.Module):
     """ Upsample block of v-net """
 
-    def __init__(self, in_channels, out_channels, num_convs, use_bottle_neck=False):
+    def __init__(self, in_channels, out_channels, num_convs, use_bottle_neck=False, use_se=False):
         super(UpBlock, self).__init__()
         self.up_conv = nn.ConvTranspose3d(in_channels, out_channels // 2, kernel_size=2, stride=2, groups=1)
         self.up_bn = nn.BatchNorm3d(out_channels // 2)
         self.up_act = nn.ReLU(inplace=True)
         if use_bottle_neck:
-            self.rblock = BottResidualBlock3(out_channels, 4, num_convs)
+            self.rblock = BottResidualBlock3(out_channels, 4, num_convs, use_se=use_se)
         else:
-            self.rblock = ResidualBlock3(out_channels, 3, 1, num_convs)
+            self.rblock = ResidualBlock3(out_channels, 3, 1, num_convs, use_se=use_se)
 
     def forward(self, input, skip):
         out = self.up_act(self.up_bn(self.up_conv(input)))
@@ -127,33 +172,69 @@ class UpBlock(nn.Module):
         return out
 
 
+class OutputBlock(nn.Module):
+    """ output block of v-net
+
+        The output is a list of foreground-background probability vectors.
+        The length of the list equals to the number of voxels in the volume
+    """
+
+    def __init__(self, in_channels, out_channels):
+        super(OutputBlock, self).__init__()
+        self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm3d(out_channels)
+        self.act1 = nn.ReLU()
+        self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size=1)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, input):
+        out = self.act1(self.bn1(self.conv1(input)))
+        out = self.conv2(out)
+        out = self.softmax(out)
+        return out
+
+
 class AttentionGate(nn.Module):
     """
-    Attention Gate 
+    Attention Gate
     """
+
     def __init__(self, in_channels, gate_channels, inner_channels, mode='softmax'):
         super(AttentionGate, self).__init__()
         self.mode = mode
-        self.conv_gate = nn.Conv3d(in_channels=gate_channels, out_channels=inner_channels, kernel_size=1, stride=1,
-                                   padding=0)
-        self.conv_fea = nn.Conv3d(in_channels=in_channels, out_channels=inner_channels, kernel_size=1, stride=1,
-                                  padding=0)
+        self.conv_gate = nn.Conv3d(in_channels=gate_channels,
+                                   out_channels=inner_channels,
+                                   kernel_size=3,
+                                   stride=1,
+                                   padding=1)
+        self.conv_fea = nn.Conv3d(in_channels=in_channels,
+                                  out_channels=inner_channels,
+                                  kernel_size=3,
+                                  stride=1,
+                                  padding=1)
+        self.bn_relu = nn.Sequential(nn.BatchNorm3d(inner_channels),
+                                     nn.ReLU())
         self.conv_mask = nn.Conv3d(in_channels=inner_channels, out_channels=1, kernel_size=3, stride=1, padding=1)
-        self.conv_W = nn.Conv3d(in_channels=in_channels, out_channels=in_channels, kernel_size=3, stride=1, padding=1)
+        self.conv_W = nn.Sequential(
+            nn.Conv3d(in_channels, in_channels, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm3d(in_channels),
+            nn.ReLU())
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, feature, gate):
         fea_x = self.conv_fea(feature)
         gating_g = self.conv_gate(gate)
+
         fea_x_size = fea_x.size()[2:]
         gating_g = F.interpolate(gating_g, size=fea_x_size, mode='trilinear', align_corners=True)
-        F_fea_gate = F.leaky_relu(gating_g + fea_x, negative_slope=0.01)
+        F_fea_gate = self.bn_relu(gating_g + fea_x)
         mask = self.conv_mask(F_fea_gate)
         # default softmax
-        mask_tmp = mask.view(mask.size(0), 1, -1)
-        mask_tmp = F.softmax(mask_tmp, dim=2)
+        mask_tmp = mask.view(mask.size(0), -1)
+        mask_tmp = F.softmax(mask_tmp, dim=1)
         mask_tmp = mask_tmp.view_as(mask)
         if self.mode == 'sigmoid':
-            mask_tmp = F.sigmoid(mask)
+            mask_tmp = self.sigmoid(mask)
         mask = mask_tmp
         mask = mask.expand_as(feature)
         fea_xl = torch.matmul(mask, feature)
@@ -184,25 +265,29 @@ def gaussian_weight_init(m, conv_std=0.01, bn_std=0.01):
         m.weight.data.normal_(1.0, bn_std)
         m.bias.data.zero_()
 
-        
-class OutputBlock(nn.Module):
-    """ output block of v-net
+# class MyTest(object):
+#     def __init__(self):
+#         self.data = torch.rand(1, 1, 32, 64, 64)
+#
+#     def test_InputBlock(self):
+#         model = InputBlock(self.data.size(1), out_channels=self.data.size(1))
+#         out = model(self.data)
+#         print(out.size() == self.data.size())
+#
+#     def test_downblock(self):
+#         model = DownBlock(1, 3)
+#         out = model(self.data)
+#         shape = self.data.size()
+#         shape = [s // 2 for s in shape[2:]]
+#         print(list(out.size())[2:] == shape)
 
-        The output is a list of foreground-background probability vectors.
-        The length of the list equals to the number of voxels in the volume
-    """
 
-    def __init__(self, in_channels, out_channels):
-        super(OutputBlock, self).__init__()
-        self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm3d(out_channels)
-        self.act1 = nn.ReLU()
-        self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size=1)
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, input):
-        out = self.act1(self.bn1(self.conv1(input)))
-        out = self.conv2(out)
-        out = self.softmax(out)
-        return out        
-        
+# if __name__ == '__main__':
+#     # test = MyTest()
+#     # test.test_downblock()
+#     gate = torch.rand(1, 16, 16, 32, 32)
+#     fea = torch.rand(1, 8, 32, 64, 64)
+#     model = AttentionGate(8, 16, 4)
+#     out = model(fea, gate)
+#     print(out.size(),fea.size())
+#
---
+++ vbnet_se.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+
+from blocks import *
+
+
+class VNet(nn.Module):
+    """ v-net for segmentation """
+
+    def __init__(self, in_channels, out_channels=2):
+        super(VNet, self).__init__()
+        self.in_block = InputBlock(in_channels, 16)
+        self.down_32 = DownBlock(16, 1, use_bottle_neck=False, use_se=True)
+        self.down_64 = DownBlock(32, 2, use_bottle_neck=True, use_se=True)
+        self.down_128 = DownBlock(64, 3, use_bottle_neck=True, use_se=True)
+        self.down_256 = DownBlock(128, 3, use_bottle_neck=True, use_se=True)
+        self.up_256 = UpBlock(256, 256, 3, use_bottle_neck=True, use_se=True)
+        self.up_128 = UpBlock(256, 128, 3, use_bottle_neck=True, use_se=True)
+        self.up_64 = UpBlock(128, 64, 2, use_bottle_neck=False, use_se=True)
+        self.up_32 = UpBlock(64, 32, 1, use_bottle_neck=False, use_se=True)
+        self.out_block = OutputBlock(32, out_channels)
+
+    def forward(self, input):
+        out16 = self.in_block(input)
+        out32 = self.down_32(out16)
+        out64 = self.down_64(out32)
+        out128 = self.down_128(out64)
+        out256 = self.down_256(out128)
+        out = self.up_256(out256, out128)
+        out = self.up_128(out, out64)
+        out = self.up_64(out, out32)
+        out = self.up_32(out, out16)
+        out = self.out_block(out)
+        return out
+
+    def max_stride(self):
+        return 16
+
+
+def test():
+    data = torch.rand(1, 1, 32, 320, 320)
+    model = VNet(1, 2)
+    out = model(data)
+    print(out.size())
+
+test()
